fa7688f8c36e361195fecb962e21650c88ecf129
Luce <157084740+lucewebster@users.noreply.github.com>
2024-05-26 18:09:35 +1000

    added comments to code

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// This function creates an popup alert when a user has subscribed to the newsletter.
 function showMessage() {
     alert("Thank you for Subscribing to our Newsletter!");
 }
 
 document.getElementById("subscribe-button").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); 
     showMessage();
 });
